0351387649e2a06f7d38783503f94f632a5bbb2e
Karbonara <37299335+karbonara@users.noreply.github.com>
2022-02-11 12:50:47 +0300

    Добавил компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Header from '../header/header';
+import CitiesLinks from '../cities-links/cities-links';
+import Cities from '../cities/cities';
+type AppCitiesProps = {
+  placesToStay: number;
+}
+function App({ placesToStay }: AppCitiesProps): JSX.Element {
+  return (
+    <div>
+      <Header />
+      <CitiesLinks />
+      <Cities placesToStay={placesToStay} />
+    </div>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACES_TO_STAY: 324,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placesToStay={Setting.PLACES_TO_STAY} />
   </React.StrictMode>,
   document.getElementById('root'));
